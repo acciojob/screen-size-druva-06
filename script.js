04217654acf1b2f6bde426b445005b00a4dbc8f9
Druvasena Reddy <66788819+druva-06@users.noreply.github.com>
2023-07-27 21:43:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+const resizeHeader = document.getElementsByTagName('h1')[0]
+
+let x = innerWidth
+let y = innerHeight
+resizeHeader.innerHTML = `Width: ${x} and Height: ${y}`
+
+window.addEventListener('resize',() => {
+    let x = innerWidth
+    let y = innerHeight
+    resizeHeader.innerHTML = `Width: ${x} and Height: ${y}`
+})
+
